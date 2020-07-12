f80f0cec337022b1af30cacc54145ef673aa175a
HansKoh <xuhanhuihans@gmail.com>
2020-07-12 13:14:59 +0800

    Updates

---
+++ precache-manifest.ee6cf8add1b9f2e3adfef42189dc7cd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e77bef39a3054c73dd1667bb73ece2",
+    "url": "/react-todolist-app./index.html"
+  },
+  {
+    "revision": "cc8c674a214efc07031c",
+    "url": "/react-todolist-app./static/css/main.43e67e12.chunk.css"
+  },
+  {
+    "revision": "5369231de70aa89e3314",
+    "url": "/react-todolist-app./static/js/2.47293ffc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist-app./static/js/2.47293ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8c674a214efc07031c",
+    "url": "/react-todolist-app./static/js/main.04aca9b0.chunk.js"
+  },
+  {
+    "revision": "06ae2e03a12900fed758",
+    "url": "/react-todolist-app./static/js/runtime-main.61ac8412.js"
+  }
+]);
